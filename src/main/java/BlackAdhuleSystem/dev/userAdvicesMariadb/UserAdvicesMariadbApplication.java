f7dee5d34de16dc80a61dd7f5d17f1d0e42f9004
ADHULE <adhulejean@gmail.com>
2025-11-05 15:14:00 +0200

    initiaze mariadb data base && smtp4dev server for email local testing

---
+++ src/main/java/BlackAdhuleSystem/dev/userAdvicesMariadb/UserAdvicesMariadbApplication.java
@@ -0,0 +1,13 @@
+package BlackAdhuleSystem.dev.userAdvicesMariadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAdvicesMariadbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAdvicesMariadbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BlackAdhuleSystem/dev/userAdvicesMariadb/UserAdvicesMariadbApplicationTests.java
@@ -0,0 +1,13 @@
+package BlackAdhuleSystem.dev.userAdvicesMariadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAdvicesMariadbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
